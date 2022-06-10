4281d3bfc335d5de3bbc3149c6df309656b82835
Vladimir_Bavtenko <batonich84@mail.ru>
2022-06-10 13:01:42 +0300

    Создает макет страницы формы входа.

--- news-project/src/components/app/app.jsx
+++ news-project/src/components/app/app.jsx
@@ -1,10 +1,12 @@
 import "./app.css";
 import { MainPage } from "../../pages/main-page/main-page";
+import { LoginPage } from "../../pages/login-page/login-page";
 
 function App() {
   return (
     <div className="App">
       <MainPage />
+      <LoginPage />
     </div>
   );
 }
