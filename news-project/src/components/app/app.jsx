94a37ab7e7686bfcda649162b4c7068bb461be8e
Vladimir_Bavtenko <batonich84@mail.ru>
2022-06-10 13:14:00 +0300

    Создает макет страницы с новостями.

--- news-project/src/components/app/app.jsx
+++ news-project/src/components/app/app.jsx
@@ -1,12 +1,14 @@
 import "./app.css";
 import { MainPage } from "../../pages/main-page/main-page";
 import { LoginPage } from "../../pages/login-page/login-page";
+import { NewsPage } from "../../pages/news-page/news-page";
 
 function App() {
   return (
     <div className="App">
       <MainPage />
       <LoginPage />
+      <NewsPage />
     </div>
   );
 }
