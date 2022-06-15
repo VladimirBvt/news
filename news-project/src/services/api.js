53260a0106a0d17b4dcc3901ecc0a0e14dc84992
Vladimir_Bavtenko <batonich84@mail.ru>
2022-06-15 19:57:09 +0300

    Добавит модуль для работы с токеном.

--- news-project/src/services/api.js
+++ news-project/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = "https://jsonplaceholder.typicode.com/posts";
+const BACKEND_URL = "https://9.react.pages.academy/guess-melody";
 const REQUEST_TIMEOUT = 5000;
 
 export const createAPI = () => {
