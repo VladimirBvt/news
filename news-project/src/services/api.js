88ea7edf97cbfb444fd06c237a2a23c1e55a1bcf
Vladimir_Bavtenko <batonich84@mail.ru>
2022-06-15 20:15:29 +0300

    Добавит поддержку токенов, обновляя конфигурацию axios с помощью перехватчика запроса.

--- news-project/src/services/api.js
+++ news-project/src/services/api.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { getToken } from "./token";
 
 const BACKEND_URL = "https://9.react.pages.academy/guess-melody";
 const REQUEST_TIMEOUT = 5000;
@@ -7,6 +8,16 @@ export const createAPI = () => {
   const api = axios.create({
     baseURL: BACKEND_URL,
     timeout: REQUEST_TIMEOUT,
+  });
+
+  api.interceptors.request.use((config) => {
+    const token = getToken();
+
+    if (token) {
+      config.headers["x-token"] = token;
+    }
+
+    return config;
   });
 
   return api;
